16e867341230f6f5feb0bddb88bd50443e767108
Billcountry <billcountrymwaniki@gmail.com>
2018-03-15 12:58:14 +0300

    Removed conflicting require

--- api/payment.php
+++ api/payment.php
@@ -1,17 +1,29 @@
 <?php
-require_once "index.php";
-$api = new Api();
+function check_array($keys, $array){
+    // This function checks whether all the required keys are set
+    $success = true;
+    foreach($keys as $key){
+        // This is the equivalent of checking if isset($_GET['name'])
+        if(!isset($array[$key])){
+            $success = false;
+            // We break out of the loop since the test has failed
+            break;
+        }
+    }
+    return $success;
+}
+
 $api_key = "7b9e472073508520c31aaff4074961bd";
 $api_sign = "gUNNFPcF8FTdwH5kGhJ883izj1ijrBUjRssg/MmByna51kUHoKyzGLtO2Jvb6FyIEnVvDmCZ6wN8Ae3zXTCuQUErN5so/yz3opOwmKwxG/YDExrsLyPpvbvl3tfYNnjE7FOIDOD7TYRGdrSP85OcIVaLmgusuma923h704RDnPs=";
-
+$conn = mysqli_connect("localhost", "rentalpo_user", "5trongPass", "rentalpo_db");
 // Keys sent by lipisha
 $initiate = ['api_key','api_signature','api_version','api_type(Initiate)','transaction','transaction_reference','transaction_type','transaction_country','transaction_method','transaction_date','transaction_currency','transaction_amount','transaction_paybill','transaction_paybill_type','transaction_account','transaction_account_number','transaction_account_name','transaction_merchant_reference','transaction_name','transaction_mobile','transaction_email','transaction_code','transaction_status'];
-if($api->check_array(['api_key','api_signature','api_version','api_type','transaction_amount','transaction_mobile','transaction_code'],$_POST)){
+if(check_array(['api_key','api_signature','api_version','api_type','transaction_amount','transaction_mobile','transaction_code'],$_POST)){
     if($_POST['api_type'] == 'Initiate'){
         // Confirm that the request is authentic
         if($_POST['api_key']==$api_key && $_POST['api_signature']==$api_sign){
             $data = json_encode($_POST, JSON_PRETTY_PRINT);
-            $stmt = $api->conn->prepare("INSERT INTO transactions (transaction_no, details, amount) VALUES (?,?,?)");
+            $stmt = $conn->prepare("INSERT INTO transactions (transaction_no, details, amount) VALUES (?,?,?)");
             $stmt->bind_param('ssd',$_POST['transaction_code'],$data,$_POST['transaction_amount']);
             $stmt->execute();
             // Response
